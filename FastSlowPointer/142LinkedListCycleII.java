dd2417b2619d0a57f617d124fcc5ccef28241a06
yash4necro <yash4167@gmail.com>
2025-11-23 11:36:13 +0530

    FastSlowPointer: 142LinkedListCycleII

--- FastSlowPointer/142LinkedListCycleII.java
+++ FastSlowPointer/142LinkedListCycleII.java
@@ -11,21 +11,16 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if (head == null) return head;
         ListNode slow = head, fast = head;
-
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            if (slow == fast) {
-                break;
-            }
+            if (slow == fast) break;
         }
         if (fast == null || fast.next == null) return null;
-        fast = head;
-        while (fast != slow) {
+        while (head != slow) {
+            head = head.next;
             slow = slow.next;
-            fast = fast.next;
         }
         return slow;
     }
