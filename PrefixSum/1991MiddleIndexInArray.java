17746e500c2dfe3ca7691cde029a12f049094c40
yash4necro <yash4167@gmail.com>
2025-12-01 13:23:52 +0530

    PrefixSum: 1991MiddleIndexInArray

--- PrefixSum/1991MiddleIndexInArray.java
+++ PrefixSum/1991MiddleIndexInArray.java
@@ -1,18 +1,20 @@
-package PrefixSum;
-
 class Solution {
     public int findMiddleIndex(int[] nums) {
-        int rightSum = 0;
-        for (int i: nums) {
-            rightSum += i;
+        int preSum [] = new int[nums.length];
+        int suffSum [] = new int[nums.length];
+
+        preSum[0] = 0; suffSum[nums.length - 1] = 0;
+
+        for (int i = 1; i < preSum.length; i++) {
+            preSum[i] = nums[i - 1] + preSum[i - 1];
         }
-        int leftSum = 0;
+
+        for (int i = suffSum.length - 2; i >= 0; i--) {
+            suffSum[i] = nums[i + 1] + suffSum[i + 1];
+        }
+
         for (int i = 0; i < nums.length; i++) {
-            rightSum -= nums[i];
-            if (leftSum == rightSum) {
-                return i;
-            }
-            leftSum += nums[i];
+            if (suffSum[i] == preSum[i]) return i;
         }
         return -1;
     }
