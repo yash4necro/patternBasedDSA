f2568f218a1f457f3c62ebda9aad4a4e2a5492ff
yash4necro <yash4167@gmail.com>
2026-02-14 20:13:14 +0530

    TwoPointer: 11ContainerWithMostWater

---
+++ TwoPointer/11ContainerWithMostWater.java
@@ -0,0 +1,15 @@
+package TwoPointer;
+
+class Solution {
+    public int maxArea(int[] height) {
+        int maxWater = Integer.MIN_VALUE;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            if (area > maxWater) maxWater = area;
+            if (height[left] < height[right]) left++;
+            else right--;
+        }
+        return maxWater;
+    }
+}
